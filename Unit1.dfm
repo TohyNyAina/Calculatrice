24395bc3daa10285aa8d86a275a7f90edd9fd719
Tohy Ny Aina <109283824+TohyNyAina@users.noreply.github.com>
2023-03-22 14:51:50 +0300

    Initial commit

---
+++ Unit1.dfm
@@ -0,0 +1,49 @@
+object Form1: TForm1
+  Left = 321
+  Top = 207
+  Width = 1044
+  Height = 540
+  Caption = 'Form1'
+  Color = clBtnFace
+  Font.Charset = DEFAULT_CHARSET
+  Font.Color = clWindowText
+  Font.Height = -11
+  Font.Name = 'MS Sans Serif'
+  Font.Style = []
+  OldCreateOrder = False
+  PixelsPerInch = 96
+  TextHeight = 13
+  object Edit1: TEdit
+    Left = 440
+    Top = 88
+    Width = 121
+    Height = 21
+    TabOrder = 0
+    Text = 'Edit1'
+  end
+  object Edit2: TEdit
+    Left = 440
+    Top = 144
+    Width = 121
+    Height = 21
+    TabOrder = 1
+    Text = 'Edit2'
+  end
+  object Button1: TButton
+    Left = 440
+    Top = 208
+    Width = 121
+    Height = 25
+    Caption = 'Button1'
+    TabOrder = 2
+    OnClick = Button1Click
+  end
+  object Edit3: TEdit
+    Left = 440
+    Top = 280
+    Width = 121
+    Height = 21
+    TabOrder = 3
+    Text = 'Edit3'
+  end
+end
---
+++ Unit1.h
@@ -0,0 +1,26 @@
+//---------------------------------------------------------------------------
+
+#ifndef Unit1H
+#define Unit1H
+//---------------------------------------------------------------------------
+#include <Classes.hpp>
+#include <Controls.hpp>
+#include <StdCtrls.hpp>
+#include <Forms.hpp>
+//---------------------------------------------------------------------------
+class TForm1 : public TForm
+{
+__published:	// Composants gérés par l'EDI
+        TEdit *Edit1;
+        TEdit *Edit2;
+        TButton *Button1;
+        TEdit *Edit3;
+        void __fastcall Button1Click(TObject *Sender);
+private:	// Déclarations de l'utilisateur
+public:		// Déclarations de l'utilisateur
+        __fastcall TForm1(TComponent* Owner);
+};
+//---------------------------------------------------------------------------
+extern PACKAGE TForm1 *Form1;
+//---------------------------------------------------------------------------
+#endif
